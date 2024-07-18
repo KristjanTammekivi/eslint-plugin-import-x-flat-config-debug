26426440daea8a006f21aa5149c2ba5fb7b7a2f6
Kristjan Tammekivi <kristjantammekivi@gmail.com>
2024-07-18 15:47:42 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const tseslint = require('typescript-eslint');
+const importPlugin = require('eslint-plugin-import-x');
+
+module.exports = tseslint.config(
+    {
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                project: true
+            }
+        }
+    },
+    {
+        plugins: {
+            import: importPlugin
+        },
+        files: ['src/*.ts'],
+        rules: {
+            'import/no-unused-modules': ['error', { unusedExports: true, missingExports: false }],
+        }
+    }
+)
